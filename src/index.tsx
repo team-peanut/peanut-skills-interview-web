1893211789eedb7ba2801dec148fcae33b99facd
James Pearson <james@teampeanut.com>
2023-06-08 19:40:07 +0100

    ✨ Initial routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import DefaultLayout from './layouts/DefaultLayout/DefaultLayout';
+
+import './index.scss';
+
+
+const router = createBrowserRouter([
+  {
+    element: <DefaultLayout />,
+    children: [{
+      path: "/",
+      element: <div>Hello world!</div>,
+    },]
+  }
+  
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
